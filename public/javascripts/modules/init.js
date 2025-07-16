51a8b83b93b97b3428084b95e8cd8f0980295096
Mahdi <mahdi.teraxler.mtr@gmail.com>
2025-07-16 09:40:40 +0330

    fix(merge bug): just remove useless code

--- public/javascripts/modules/init.js
+++ public/javascripts/modules/init.js
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
 import getDB from"../apis/db.api.js";import{getFromLocalStorage,saveToLocalStorage}from"./utils.js";async function saveDefaultData(){let a=getFromLocalStorage("DB");null==a&&(a=await getDB(),saveToLocalStorage("DB",a))}window.addEventListener("load",(async()=>{await saveDefaultData()}));
-=======
-import getDB from "../apis/db.api.js";
-import { getFromLocalStorage, saveToLocalStorage } from "./utils.js";
-
-async function saveDefaultData() {
-  let DB = getFromLocalStorage("DB");
-  if (DB == null) {
-    DB = await getDB();
-
-    saveToLocalStorage("DB", DB);
-  }
-}
-
-window.addEventListener("load", async () => {
-  await saveDefaultData();
-});
->>>>>>> 9ed8f5af226bc334d4a671902162e6d3dce9d127
